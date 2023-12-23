66d83be1d8461ab7dbfa036d85503b7ecf8c2d88
Alexander Rodriguez-Zingg <alexrodriguezzingg@icloud.com>
2023-12-22 18:37:10 -0700

    Completed guided projects and passed all tests

--- src/com/bloomtech/socialfeed/repositories/PostRepository.java
+++ src/com/bloomtech/socialfeed/repositories/PostRepository.java
@@ -1,14 +1,24 @@
 package com.bloomtech.socialfeed.repositories;
 
+import com.bloomtech.socialfeed.helpers.LocalDateTimeAdapter;
 import com.bloomtech.socialfeed.models.Post;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,25 +29,30 @@ public class PostRepository {
     public PostRepository() {
     }
 
+    private Gson createGsonWithAdapter() {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .setPrettyPrinting()
+                .create();
+    }
+
     public List<Post> getAllPosts() {
         //TODO: return all posts from the PostData.json file
         List<Post> allPosts = new ArrayList<>();
+        Gson gson = createGsonWithAdapter();
+
         try {
             String json = new String(Files.readAllBytes(Paths.get(POST_DATA_PATH)));
             if (json == null || json.isEmpty()) {
-                return new ArrayList<>();
+                return allPosts;
             }
-            Gson gson = new Gson();
             Type postType = new TypeToken<ArrayList<Post>>(){}.getType();
             allPosts = gson.fromJson(json, postType);
-            if (allPosts == null) {
-                return new ArrayList<>();
-            }
-            return allPosts;
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        return allPosts;
     }
 
     public List<Post> findByUsername(String username) {
@@ -47,15 +62,14 @@ public class PostRepository {
                 .collect(Collectors.toList());
     }
 
-    public List<Post> addPost(Post post) {
+    public List<Post> addPost(Post newPost) {
         List<Post> allPosts = getAllPosts();
-        allPosts.add(post);
+        allPosts.add(newPost);
 
         //TODO: Write the new Post data to the PostData.json file
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
+        Gson gson = createGsonWithAdapter();
         String json = gson.toJson(allPosts);
+
         try {
             Files.write(Paths.get(POST_DATA_PATH), json.getBytes());
         } catch (IOException e) {
